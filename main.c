0b1d2fda2376d82c5ff7d844d13f4613cc79d5c7
Dontos Shirely <dshirely@ox-v2.msk.21-school.ru>
2022-04-09 19:27:36 +0300

    9/04

--- main.c
+++ main.c
@@ -1,20 +1,74 @@
 #include "philo.h"
-void parser(int argc, char **argv, t_gen *gen)
+int ft_check_input(t_table *table)
 {
-	gen->num=ft_atoi(argv[1]);
-	gen->t_die=ft_atoi(argv[2]);
-	gen->t_eat=ft_atoi(argv[3]);
-	gen->t_sleep=ft_atoi(argv[4]);
-	if(argc == 6)
-		// gen->
+	if(table->num < 1 || table->t_die <  60 || table->t_eat < 60 || \
+		table->t_sleep < 60)
+	{
+		printf("error\nCheck your input!\n");
+		return (1);
+	}
+	return (0);
 }
+void parser(int argc, char **argv, t_table *table)
+{
+	table->num=ft_atoi(argv[1]);
+	table->t_die=ft_atoi(argv[2]);
+	table->t_eat=ft_atoi(argv[3]);
+	table->t_sleep=ft_atoi(argv[4]);
+	if(argc == 6)
+		table->num =ft_atoi(argv[5]);
+
+}
+
+void mutex_creator(t_table *table)
+{
+	int i;
+
+	i = 0;
+	table->vilki=(pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * table->num);
+	if(!table->vilki)
+	{
+		printf("Mutex error\n");
+		return ;
+	}
+	while(i < table->num)
+	{
+		if(pthread_mutex_init(&table->vilki[i++],NULL) != 0)
+		{
+			printf("mutex_init error\n");
+			return ;
+		}
+	}
+}
+
+
+void philo_creator(t_table *table)//here we're making threads
+{
+	table->philos=(t_phil *)malloc(sizeof(t_phil) * table->num);
+	
+}
+// void gettime(t_table *table)
+// {
+
+// }
 
 int main(int argc,char**argv)
 {
-	t_gen gen;
+	t_table table;
 
 	if (argc == 5 || argc == 6)
 	{
-		parser(argc,argv,&gen);
+		parser(argc,argv,&table);
+		if(ft_check_input(&table))
+			return (1);
+		mutex_creator(&table);
+		// gettime(&table);
+		philo_creator(&table);	
+		sleep(1000);
+	}
+	else
+	{
+		printf("Incorrect input\n");
+		return (0);
 	}
 }
--- philo.h
+++ philo.h
@@ -1,36 +1,36 @@
-#ifndef PHLIHO_H
+#ifndef PHILO_H
 # define PHILO_H
 #include <pthread.h>
 #include <sys/time.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <libft/libft.h>
+#include "libft/libft.h"
 
 typedef struct s_phil
 {
-	pthread_mutex_t	*lf;
-	pthread_mutex_t	*rg;
-	pthread_t		self;
-	struct timeval	tek;
-	struct timeval	last;
-	int				n;
-	int				rep;
-	struct s_gen	*data;
+	pthread_mutex_t	*min_vilka;
+	pthread_mutex_t	*max_vilka;
+	pthread_t		id;
+	// struct timeval	tek;
+	// struct timeval	last;
+	// int				n;
+	// int				rep;
+	struct s_table	*table;
 }				t_phil;
  
-typedef struct s_gen
+typedef struct s_table
 {
 	int				t_eat;
 	int				t_sleep;
 	int				t_die;
 	int				num;
-	int				rep;
-	int				doa;
+	// int				rep;
+	// int				doa;
 	pthread_mutex_t	*vilki;
-	pthread_mutex_t	printm;
-	t_phil			*phils;
+	// pthread_mutex_t	printm;
+	t_phil			*philos;
 	struct timeval	start;
-}				t_gen;
+}				t_table;
 
 #endif
